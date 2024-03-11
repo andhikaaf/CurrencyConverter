23721f2429869d3e52b394142d60ec01de9d0dc2
Andhika Alty Firdaus <andhika.a.firdaus@gmail.com>
2024-03-11 19:23:34 +0900

    Initial commit

---
+++ js/main/converter.js
@@ -0,0 +1,47 @@
+
+
+export default class Converter
+{
+    constructor(idbManager)
+    {
+        //the idb database to retrieve and save data to and from CACHE database
+        this._idbManager = idbManager;
+    }
+
+    getAllCurrencies(callBack)
+    {
+        fetch("http://api.exchangeratesapi.io/v1/latest?access_key=3d1ebf44fb89684d580639c914bf2c43&format=1")
+        .then(response => callBack(null, response))
+        .catch(error => callBack(error, null));
+    }
+
+    //Converts the currency
+    convertCurrency(amount, fromCurrency, toCurrency, callBack)
+    {
+        fromCurrency = encodeURIComponent(fromCurrency);
+        toCurrency = encodeURIComponent(toCurrency);
+        const query = fromCurrency + ',' + toCurrency;
+
+        //we build the URL
+         const url = `http://api.exchangeratesapi.io/v1/latest?access_key=3d1ebf44fb89684d580639c914bf2c43&format=1&symbols=${query}`;
+       
+         fetch(url)
+            .catch(error => callBack(error))
+            .then(results => 
+                {
+                    //Invoke's the call back method of the upper layer using this class after 
+                    //converting the result to json.
+                    results.json().then(jsonData => 
+                        {
+                           
+                         
+                           let fromValue = jsonData["rates"].JPY;
+                           let toValue = jsonData["rates"].IDR;
+                           let total = toValue / fromValue * amount * 1.1;                     
+                           callBack(null, (Math.ceil(total / 1000) * 1000));
+
+                        });
+                });            
+    }
+}
+
---
+++ js/main/main.js
@@ -0,0 +1,155 @@
+import Converter from './converter.js';
+import ServiceWorker from './serviceWorker.js';
+import IDBManager from './idbManager.js';
+
+
+//Let's register the service worker.
+let sw = new ServiceWorker();
+sw.registerServiceWorker('./sw.js');
+
+//Create an IDb manager object for index db transactions
+let idbMan = new IDBManager();
+
+const submitButton = document.getElementById("submit_button");
+const toSelect = document.getElementById("currency_to_dropdown");
+const toSelect2 =document.getElementById("currency_to_dropdown");
+const fromSelect = document.getElementById("currency_from_dropdown");
+const fromSelect2 =document.getElementById("currency_from_dropdown");
+const amountEntry = document.getElementById('amount_entry');
+const convertedValueEntry = document.getElementById('converted_value_entry');
+const exchangeIcon = document.getElementById('exchange_icon');
+const dimmerUno = document.getElementById('dimmer_uno');
+const todayRate = document.getElementById('today_rate');
+
+//create a converter object
+let converter = new Converter(idbMan);
+
+//Get all the currencies, then add them to the Drop downs on the index.html page
+converter.getAllCurrencies( (error, response) => 
+        { 
+            if (response)
+            {
+                response.json().then((jsonData) => {
+                    let todayRate = Math.floor(jsonData["rates"].IDR / jsonData["rates"].JPY * 100)/100;
+
+                    updateRate(todayRate);
+                });
+            }
+
+            setLoading();
+            // if(response)
+            // {
+            //     response.json().then((jsonData) => {
+            //         let data = jsonData.results;
+            //         let set = {data};
+                
+            //         Object.keys(jsonData.results).forEach((key,index) => {
+                    
+            //            let currency = jsonData.results[key];
+            //            let option1 = document.createElement("option");
+            //            let option2 = document.createElement("option");
+
+            //             //format the string which will be displayed in each
+            //             //drop down's options.
+            //             if(!currency.currencySymbol)
+            //             {    
+            //                 option1.text = `(${currency.id}) ${currency.currencyName}`;
+            //                 option2.text = `(${currency.id}) ${currency.currencyName}`;
+            //             }
+                    
+            //             else
+            //             {
+            //                 option1.text = `(${currency.id}) ${currency.currencyName} ${currency.currencySymbol}`;
+            //                 option2.text = `(${currency.id}) ${currency.currencyName} ${currency.currencySymbol}`;
+            //             }
+
+            //             //Add currencies to both drop downs
+            //             option1.value = currency;
+            //             option2.value = currency;
+            //             toSelect.add(option1, null);
+            //             fromSelect.add(option2, null);
+            //         });
+            //     });
+
+                
+
+            // }
+            // else if(error)
+            // {
+            //     alert("An error occured while fetching the currencies.");
+            // }
+
+          
+
+            let option3 = document.createElement("option");
+            let option4 = document.createElement("option");
+            option3.value = 'IDR';
+            option4.value ='JPY';
+            option3.text = '(IDR) Indonesian Rupiah';
+            option4.text = '(JPY) Japanese Yen';
+            
+
+            toSelect2.add(option3,null);
+            fromSelect2.add(option4, null);
+
+        
+
+
+            endLoading();
+        });
+        
+
+//Listen to when the submit button is clicked
+submitButton.addEventListener("click", () => 
+{
+    let amount = amountEntry.value;
+
+    if(amount)
+    {        
+        let fromCurrency = fromSelect.options[fromSelect.selectedIndex].innerHTML;
+        let toCurrency = toSelect.options[toSelect.selectedIndex].innerHTML;
+
+        let regExp = /\(([^)]+)\)/;
+        let fromCurrencyKey = regExp.exec(fromCurrency)[1];
+        let toCurrencyKey = regExp.exec(toCurrency)[1];
+
+        if(fromCurrencyKey == toCurrencyKey)
+        {
+            convertedValueEntry.value = amount;
+        }
+        else
+        {
+            setLoading();
+            converter.convertCurrency(amount, fromCurrencyKey, toCurrencyKey, (error, result) => 
+            {               
+                if(result)
+                {
+                    convertedValueEntry.value = result;                 
+                }
+                else
+                {
+                    alert("An error occured while converting. Please do the conversion online first, before working offline as long as you wish.");
+                }
+                endLoading();
+            });
+        }
+    }
+    else
+    {
+        alert("Please enter the amount which you wish to convert");
+    }
+});
+
+function setLoading(){
+    exchangeIcon.style.visibility = 'hidden';
+    dimmerUno.style.visibility = 'visible';
+}
+function endLoading(){
+    exchangeIcon.style.visibility = 'visible';
+    dimmerUno.style.visibility = 'hidden';
+}
+
+function updateRate(element)
+{
+    todayRate.innerHTML = element;
+}
